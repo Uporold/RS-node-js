b6732cdd2d99613b9b8fd2264081faee43f11963
Uporold <crubick1337@gmail.com>
2021-05-07 16:07:31 +0300

    feat: finish cli

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -1,4 +1,8 @@
 import {program} from 'commander';
+import fs from 'fs';
+import {pipeline} from 'stream';
+import {Validator} from "./src/validator.js";
+import {TextTransform} from "./src/text-transform.js";
 
 program
   .description('Caesar cipher CLI')
@@ -8,7 +12,21 @@ program
   .option('-o, --output <value>' , "Output file [Optional]")
   .parse(process.argv);
 
-console.log(program.opts())
+const { shift, action, input, output } = program.opts();
 
+new Validator(program.opts()).init();
+
+const readStream = input ? fs.createReadStream(input) : process.stdin;
+const writeStream = output ? fs.createWriteStream(output, { flags: 'a' }) : process.stdout;
+const transform = new TextTransform(shift, action);
+
+pipeline(
+  readStream,
+  transform,
+  writeStream,
+  (err) => {
+    err && process.stderr.write(err);
+  }
+)
 
 
