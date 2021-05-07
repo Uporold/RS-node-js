e26dd31ac3009519fd271d971d315802424d6759
Uporold <crubick1337@gmail.com>
2021-05-07 16:01:18 +0300

    feat: add CLI options validation and update const.js

--- src/const.js
+++ src/const.js
@@ -4,3 +4,18 @@ export const Action = {
   ENCODE: 'encode',
   DECODE: 'decode',
 };
+
+export const ErrorText = {
+  SHIFT: 'Shift required and must be only integer number, but you provide',
+  ACTION: 'Action required and must be only decode or encode, but you provide',
+  ACCESS: 'Can`t access file in path',
+  NO_FILE: 'No such file in path',
+  NO_SHIFT: 'No shift value provided',
+  NO_ACTION: 'No action value provided',
+  DIR_OPERATION: 'Illegal operation on a directory'
+}
+
+export const Mode = {
+  READ: 'read',
+  WRITE: 'write',
+}
